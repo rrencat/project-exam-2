39815b1180ed6bf60f4341e63ed72417cf388c34
rrencat <104782737+rrencat@users.noreply.github.com>
2024-02-07 20:07:06 +0100

    added api

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,3 +2,4 @@ const BASE_URL = "https://api.noroff.dev/api/v1";
 export const VENUES_URL = `${BASE_URL}/holidaze/venues`;
 export const LOGIN_URL = `${BASE_URL}/holidaze/auth/login`;
 export const REGISTER_URL= `${BASE_URL}/holidaze/auth/register`;
+export const PROFILE_URL = `${BASE_URL}/holidaze/profiles/`;
